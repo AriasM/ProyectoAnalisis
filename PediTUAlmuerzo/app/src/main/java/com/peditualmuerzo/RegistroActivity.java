e30db68bf0f2d0a75ecdb264088adf972081c4f2
SMVictor <victor.salvatierra@outlook.com>
2016-12-07 18:16:52 -0600

    Validación

--- PediTUAlmuerzo/app/src/main/java/com/peditualmuerzo/RegistroActivity.java
+++ PediTUAlmuerzo/app/src/main/java/com/peditualmuerzo/RegistroActivity.java
@@ -8,6 +8,10 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.mobsandgeeks.saripaar.ValidationError;
+import com.mobsandgeeks.saripaar.Validator;
+import com.mobsandgeeks.saripaar.annotation.Email;
+import com.mobsandgeeks.saripaar.annotation.NotEmpty;
 import com.peditualmuerzo.dataAccess.PedidoData;
 import com.peditualmuerzo.dataAccess.PedidoDataFirebase;
 import com.peditualmuerzo.dataAccess.UsuarioData;
@@ -24,15 +28,23 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 
-public class RegistroActivity extends Activity {
+public class RegistroActivity extends Activity implements Validator.ValidationListener{
 
+
+    @NotEmpty(message = "Debe ingresar su nombre")
     private EditText etNombre;
+    @NotEmpty(message = "Debe ingresar su primer apellido")
     private EditText etPrimerApellido;
+    @NotEmpty(message = "Debe ingresar su segundo apellido")
     private EditText etSegundoApellido;
+    @Email(message = "Debe ingresar un correo válido")
     private EditText etCorreoElectronico;
+    @NotEmpty(message = "Debe ingresar su nombre de usuario")
     private EditText etUsuario;
+    @NotEmpty(message = "Debe ingresar su contraseña")
     private EditText etContrasena;
     private Button btnRegistrar;
+    private Validator validator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,10 +57,18 @@ public class RegistroActivity extends Activity {
         this.etCorreoElectronico = (EditText) findViewById(R.id.etCorreoElectronico);
         this.etUsuario = (EditText) findViewById(R.id.etUsuario);
         this.etContrasena = (EditText) findViewById(R.id.etContrasena);
+        this.validator = new Validator(this);
+        this.validator.setValidationListener(this);
         this.btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
+        this.btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validator.validate();
+            }
+        });
     }
 
-    public void nuevoUsuarioServicio(View view){
+    public void nuevoUsuarioServicio(){
 
         UsuarioDataFireBase usuarioData = new UsuarioDataFireBase(this);
         UsuarioPresenter usuarioPresenter = new UsuarioPresenter(usuarioData);
@@ -72,5 +92,28 @@ public class RegistroActivity extends Activity {
         etUsuario.setText("");
         etContrasena.setText("");
     }
+    @Override
+    public void onValidationSucceeded() {
 
+        nuevoUsuarioServicio();
+
+    }
+
+    @Override
+    public void onValidationFailed(List<ValidationError> errors) {
+
+        for (ValidationError error : errors) {
+            View view = error.getView();
+            String message = error.getCollatedErrorMessage(this);
+
+            // Display error messages ;)
+            if (view instanceof EditText) {
+                ((EditText) view).setError(message);
+            } else {
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        }
+
+        Toast.makeText(this, "Debe llenar todos los campos", Toast.LENGTH_LONG).show();
+    }
 }
